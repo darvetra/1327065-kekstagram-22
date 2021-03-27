20d98625c9f54e6dd5152afc56f83303dfe7a5e2
Darvetra <darvetra13@gmail.com>
2021-03-27 10:12:25 +0300

    значения адресов сервера вынесены в константу

--- js/create-fetch.js
+++ js/create-fetch.js
@@ -1,10 +1,13 @@
 'use strict'
 
+const KEKSTAGRAM_SERVER = 'https://22.javascript.pages.academy/kekstagram';
+const KEKSTAGRAM_DATA = 'https://22.javascript.pages.academy/kekstagram/data';
+
 import {showAlert} from './render-messages.js';
 
 /* Получение данных с сервера */
 const getData = (onSuccess, onError) => {
-  fetch('https://22.javascript.pages.academy/kekstagram/data')
+  fetch(KEKSTAGRAM_DATA)
     .then((response) => {
       if (response.ok) {
         return response.json();
@@ -25,7 +28,7 @@ const getData = (onSuccess, onError) => {
 /* отправка данных на сервер */
 const sendData = (onSuccess, onFail, body) => {
   fetch(
-    'https://22.javascript.pages.academy/kekstagram',
+    KEKSTAGRAM_SERVER,
     {
       method: 'POST',
       body,
